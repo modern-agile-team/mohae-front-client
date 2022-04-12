8f10a03b61b5e92146e7a901069ce0a30dbb9b8c
Lami <Halam.Lee.Lami@gmail.com>
2022-04-12 16:11:05 +0900

    Add(halam/공통 스타일-feature/styles):colors 추가 #4

--- app/src/styles/style/palette.ts
+++ app/src/styles/style/palette.ts
@@ -1,3 +1,25 @@
+import { css } from '@emotion/css';
+
+const colors: obj = {
+  main: '#ff445e',
+  darker: '#D12A41',
+  lighter: '#FFA1AF',
+  subtle: '#FCF3F4',
+  error: '#FF3B3B',
+  info: '#0063F7',
+  success: '#06C270',
+  dark1: '#4F4E5C',
+  dark2: '#84838D',
+  dark3: '#A7A7AD',
+  dark4: '#CACACE',
+  light1: '#F9F9F9',
+  light2: '#F5F5F5',
+  light3: '#EDEDEF',
+  light4: '#E7E7E8',
+  white: '#FFFFFF',
+  black: '#000000',
+};
+
 const radius: object = {
   5: css`
     border-radius: 5px;
@@ -12,3 +34,5 @@ const radius: object = {
     border-radius: 50%;
   `,
 };
+
+export { colors, radius };

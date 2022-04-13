cec34bdeba8fd745a1da53719fab7490d763df08
leephoter <estarg51@gmail.com>
2022-04-13 14:21:08 +0900

    Add(hangyeol/컴포넌트 생성-feature/components):header, main, footer 레이아웃 생성 및 App 에 적용

--- app/src/styles/Layout/index.tsx
+++ app/src/styles/Layout/index.tsx
@@ -1,31 +1,31 @@
-// import { css, cx } from '@emotion/css';
-// // import Header from './header/Header';
-// // import Footer from './footer/Footer';
-// // import Main from './main/Main';
-// import { ReactElement } from 'react';
+import { css, cx } from '@emotion/css';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import Main from './main/Main';
+import { ReactElement } from 'react';
 
-// const grid = css`
-//   display: grid;
-//   width: 100vw;
-//   min-width: 700px;
-//   align-items: center;
-//   grid-gap: 0.5px;
-//   grid-template-columns: repeat(24, 1fr);
-//   background-color: #f9f9f8;
-// `;
+const grid = css`
+  display: grid;
+  width: 100vw;
+  min-width: 1200px;
+  align-items: center;
+  grid-gap: 0.5px;
+  grid-template-columns: repeat(24, 1fr);
+  background-color: #f9f9f8;
+`;
 
-// type Props = {
-//   props: any;
-// };
+type Props = {
+  component: any;
+  name?: string;
+  children?: any;
+};
 
-// export default function Layout(props: Props): ReactElement {
-//   return (
-//     <div>1</div>
-//     // <div className={cx(grid)}>
-//     //   <Header>{'Header'}</Header>
-//     //   <Main>{props.children}</Main>
-//     //   <Footer>{'Footer'}</Footer>
-//     // </div>
-//   );
-// }
-export {};
+export default function Layout(props: Props): ReactElement {
+  return (
+    <div className={cx(grid)}>
+      <Header>{'Header'}</Header>
+      <Main>{props.component}</Main>
+      <Footer>{'Footer'}</Footer>
+    </div>
+  );
+}
--- app/src/styles/index.ts
+++ app/src/styles/index.ts
@@ -1,3 +1,4 @@
 import { colors, aligns, sort, radius, font, shadows } from './style/palette';
+import Layout from './Layout';
 
-export { colors, aligns, sort, radius, font, shadows };
+export { colors, aligns, sort, radius, font, shadows, Layout };

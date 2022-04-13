cf2493fa96d58b4d644739b24f29128b9ea75efb
leephoter <estarg51@gmail.com>
2022-04-13 12:57:10 +0900

    Add(hangyeol/컴포넌트 생성-feature/components):컴포넌트 테스트 페이지 생성 #10

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import Layout from './styles/Layout/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/hg" element={<div>123</div>} />
+        <Route path="/hl" element={<div>{'하람 테스트 페이지'}</div>} />
+        <Route path="/sj" element={<div>{'성제 테스트 페이지'}</div>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ app/src/styles/Layout/index.tsx
@@ -0,0 +1,31 @@
+// import { css, cx } from '@emotion/css';
+// // import Header from './header/Header';
+// // import Footer from './footer/Footer';
+// // import Main from './main/Main';
+// import { ReactElement } from 'react';
+
+// const grid = css`
+//   display: grid;
+//   width: 100vw;
+//   min-width: 700px;
+//   align-items: center;
+//   grid-gap: 0.5px;
+//   grid-template-columns: repeat(24, 1fr);
+//   background-color: #f9f9f8;
+// `;
+
+// type Props = {
+//   props: any;
+// };
+
+// export default function Layout(props: Props): ReactElement {
+//   return (
+//     <div>1</div>
+//     // <div className={cx(grid)}>
+//     //   <Header>{'Header'}</Header>
+//     //   <Main>{props.children}</Main>
+//     //   <Footer>{'Footer'}</Footer>
+//     // </div>
+//   );
+// }
+export {};

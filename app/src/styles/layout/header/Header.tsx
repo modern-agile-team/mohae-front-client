ddba7e8659d08f15281ca5759d83ecef7bec2da1
leephoter <estarg51@gmail.com>
2022-04-26 14:22:49 +0900

    Fix(hangyeol/공통 스타일-feature/styles):메인 레이아웃 height 값  수정 #4

--- app/src/styles/layout/header/Header.tsx
+++ app/src/styles/layout/header/Header.tsx
@@ -2,13 +2,10 @@ import { css, cx } from '@emotion/css';
 import { color, radius, font, shadow } from '../../style/palette';
 import { ReactElement } from 'react';
 
-const header = css`
+const wrapper = css`
   background-color: white;
   position: sticky;
   top: 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   width: 100%;
   ${shadow.normal}
   @media (max-width: 1200px) {
@@ -17,8 +14,23 @@ const header = css`
   @media (min-width: 1200px) {
     padding: 0 calc((100vw - 1128px) / 2);
   }
-  height: 59px;
+  min-height: 59px;
   overflow: hidden;
+`;
+
+const content = css`
+  width: 100%;
+  height: 100%;
+  padding: 8px 0;
+  background-color: gray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const logo = css`
+  width: 40px;
+  height: 43px;
 `;
 
 type Props = {
@@ -28,5 +40,12 @@ type Props = {
 };
 
 export default function Header(props: Props): ReactElement {
-  return <div className={cx(header)}>{props.children}</div>;
+  return (
+    <div className={cx(wrapper)}>
+      <div className={content}>
+        {props.children}
+        <button className={cx(logo)}>{'로그인'}</button>
+      </div>
+    </div>
+  );
 }

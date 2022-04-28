ece79720dfc75c09ba386026d0041845aee549e1
김성제 <rlatjdwp0211@naver.com>
2022-04-28 10:35:38 +0900

    Modify(sungje/feature-components):이미지가 들어가는 버튼들 수정 #10

--- app/src/components/button/textBtn/Btn7.tsx
+++ app/src/components/button/textBtn/Btn7.tsx
@@ -2,6 +2,7 @@ import { css, cx } from '@emotion/css';
 import { color, btnStyle, font, shadow, Props } from '../index';
 
 function Btn7(props: Props) {
+  const { disable, children } = props;
   const commonStyle = css`
     ${btnStyle.square}
     width: 170px;
@@ -10,14 +11,14 @@ function Btn7(props: Props) {
     color: white;
     ${font.size[14]}
     ${font.weight.regular}
-    ${shadow.normal}
+    ${shadow.normal};
 
     &:active {
       background-color: ${color.darker};
     }
   `;
 
-  const disable = css`
+  const disableState = css`
     cursor: unset;
     background-color: ${color.light4};
     &:active {
@@ -25,11 +26,23 @@ function Btn7(props: Props) {
     }
   `;
 
+  const imgStyle = css`
+    width: 20px;
+    height: 20px;
+    margin-left: 9.67px;
+  `;
+
   const show = () =>
-    props.disable ? (
-      <button className={cx(commonStyle, disable)}>{props.children}</button>
+    disable ? (
+      <button className={cx(commonStyle, disableState)}>
+        {children}
+        <img src={'img/move.png'} className={cx(imgStyle)} />
+      </button>
     ) : (
-      <button className={cx(commonStyle)}>{props.children}</button>
+      <button className={cx(commonStyle)}>
+        {children}
+        <img src={'img/move.png'} className={cx(imgStyle)} />
+      </button>
     );
 
   return show();

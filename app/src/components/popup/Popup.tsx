609d34fddb18c96e4077cfe48de28716f5763891
Lami <Halam.Lee.Lami@gmail.com>
2022-05-17 14:08:01 +0900

    Modify(halam/컴포넌트 생성-feature/components): 팝업 컴포넌트 내부에서 visible 판단 #10

--- app/src/components/popup/Popup.tsx
+++ app/src/components/popup/Popup.tsx
@@ -1,4 +1,3 @@
-import ReactDOM from 'react-dom';
 import { css, cx } from '@emotion/css';
 import { radius, shadow, font } from '../../styles';
 
@@ -6,73 +5,74 @@ interface Props {
   [key: string]: any;
 }
 
-const wrapper = css`
-  background-color: white;
-  ${radius[6]}
-  ${shadow.normal}
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 360px;
-  height: 205px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  z-index: 9999;
-`;
+function Popup({ text1, text2, rightBtn, leftBtn, visible }: Props) {
+  const container = css`
+    visibility: ${visible ? 'visible' : 'hidden'};
+  `;
 
-const text = css`
-  width: 230px;
-  height: 24px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  const box = css`
+    background-color: white;
+    ${radius[6]}
+    ${shadow.normal}
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 360px;
+    height: 205px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 3;
+  `;
 
-  ${font.size[14]}
-  ${font.weight.regular}
-`;
+  const text = css`
+    width: 230px;
+    line-height: 23.8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    ${font.size[14]}
+    ${font.weight.regular}
+  `;
 
-const btnWrapper = css`
-  margin-top: 32px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+  const btnWrapper = css`
+    margin-top: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
 
-const subBtn = css`
-  margin-right: 8px;
-`;
+  const left = css`
+    margin-right: 8px;
+  `;
 
-const overlay = css`
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  z-index: 8888;
-`;
+  const overlay = css`
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 2;
+  `;
 
-function Popup({ text1, text2, btnMain, btnSub }: Props) {
-  const popup = document.getElementById('popup')!;
-  return ReactDOM.createPortal(
-    <>
-      <div className={cx(wrapper)}>
-        <div className={cx(text)}>{text1}</div>
-        {text2 && (
-          <>
-            <div className={cx(text)}>{text2}</div>
-          </>
-        )}
+  return (
+    <div className={cx(container)}>
+      <div className={cx(box)}>
+        <div className={cx(text)}>
+          {text1}
+          <br />
+          {text2}
+        </div>
         <div className={cx(btnWrapper)}>
-          {btnSub && <div className={cx(subBtn)}>{btnSub}</div>}
-          <div>{btnMain}</div>
+          {leftBtn && <div className={cx(left)}>{leftBtn}</div>}
+          <div>{rightBtn}</div>
         </div>
       </div>
       <div className={cx(overlay)}></div>
-    </>,
-    popup
+    </div>
   );
 }
 

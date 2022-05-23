4332441b5266085d075f16dfa278cd03403104d5
김성제 <rlatjdwp0211@naver.com>
2022-05-23 15:51:46 +0900

    Refactor(sungje/feature-components): 컴포넌트 props 받는 법 수정 후 after요소로 설명란 띄우기 #10

--- app/src/components/markbox/MarkBox.tsx
+++ app/src/components/markbox/MarkBox.tsx
@@ -2,52 +2,58 @@ import { css, cx } from '@emotion/css';
 import React, { ReactChild } from 'react';
 import { color, font, radius, shadow, btnStyle } from '../../styles';
 import { Props } from '../button';
+import Img from '../img/Img';
 
-function MarkBox(props: Props) {
-  const { helpMeAble, helpMeDisable, big, small, hover, disable } = props;
-  const imgURL = {
-    helpMeAble: 'img/exclamation-mark-main.png',
-    helpMeDisable: 'img/exclamation-mark-dark1.png',
-    helpMeBtnDisable: 'img/exclamation-mark-light1.png',
-    helpUAble: 'img/question-mark-main.png',
-    helpUDisable: 'img/question-mark-dark1.png',
-    helpUBtnDisable: 'img/question-mark-light1.png',
-  };
+interface MarkBoxProps extends Props {
+  style?: { [key: string]: string };
+}
 
-  const commonStyle = css`
-    cursor: pointer;
-    border-radius: 1px;
-  `;
+function MarkBox(props: MarkBoxProps) {
+  const { style, big, small, hover } = props;
 
-  const hoverBoxStyle = css`
-    width: 212px;
-    height: 104px;
-    ${btnStyle.square}
-    background-color: white;
-    color: ${color.dark1};
-    ${font.size[14]}
-    ${font.weight.regular}
-    line-height: 23.8px;
-    text-align: center;
-    visibility: hidden;
-    position: relative;
-    padding: 16px;
-    ${shadow.normal}
-  `;
+  const commonStyle = () => {
+    const common = css`
+      border-radius: 1px;
+      clear: both;
+      position: relative;
+      :after {
+        width: 212px;
+        height: 104px;
+        box-sizing: border-box;
+        display: block;
+        ${btnStyle.square}
+        background-color: white;
+        color: ${color.dark1};
+        ${font.size[14]}
+        ${font.weight.regular}
+        line-height: 23.8px;
+        text-align: center;
+        visibility: hidden;
+        ${shadow.normal}
+        position: relative;
+      }
+    `;
 
-  const hoverBoxAttr = () =>
-    big
+    return style && style.shape === '?'
       ? css`
-          ${hoverBoxStyle}
-          bottom: 124px;
-          left: 20px;
+          ${common}
+          ::after {
+            content: '재능을 가지고 있는 사람들에게 도움을 요청할래요.';
+            bottom: ${big ? '124px' : '120px'};
+            left: ${big ? '20px' : '16px'};
+            padding: 9px;
+          }
         `
       : css`
-          ${hoverBoxStyle}
-          bottom: 120px;
-          left: 16px;
+          ${common}
+          ::after {
+            content: '재능을 가지고 사람들에게 도움을 줄래요.';
+            bottom: ${big ? '124px' : '120px'};
+            left: ${big ? '20px' : '16px'};
+            padding: 23px;
+          }
         `;
-
+  };
   const markBoxSize = () =>
     big
       ? css`
@@ -59,84 +65,51 @@ function MarkBox(props: Props) {
           height: 24px;
         `;
 
-  const wrap = css`
-    ${markBoxSize()};
-    &:hover p {
-      visibility: visible;
-    }
-  `;
+  const wrap = () =>
+    hover
+      ? css`
+          ${markBoxSize()};
+          &:hover :after {
+            visibility: visible;
+          }
+        `
+      : css`
+          ${markBoxSize()};
+          &:hover :after {
+            visibility: hidden;
+          }
+        `;
 
   interface Attrs {
-    [state: string]: string;
+    [shape: string]: string;
   }
 
   const attrs: Attrs = {
-    helpMeAble: css`
-      background: no-repeat center / contain url(${imgURL.helpMeAble});
-    `,
-    helpMeDisable: css`
-      background: no-repeat center / contain url(${imgURL.helpMeDisable});
-    `,
-    helpMeBtnAble: css`
-      background: no-repeat center / contain url(${imgURL.helpMeAble});
-    `,
-    helpMeBtnDisable: css`
-      background: no-repeat center / contain url(${imgURL.helpMeBtnDisable});
-    `,
-    helpUAble: css`
-      background: no-repeat center / contain url(${imgURL.helpUAble});
-    `,
-    helpUDisable: css`
-      background: no-repeat center / contain url(${imgURL.helpUDisable});
-    `,
-    helpUBtnAble: css`
-      background: no-repeat center / contain url(${imgURL.helpUAble});
-    `,
-    helpUBtnDisable: css`
-      background: no-repeat center / contain url(${imgURL.helpUBtnDisable});
-    `,
+    '!able': 'img/exclamation-mark-main.png',
+    '!disable': 'img/exclamation-mark-dark1.png',
+    '?able': 'img/question-mark-main.png',
+    '?disable': 'img/question-mark-dark1.png',
   };
 
-  const attrProps = [
-    ...Object.keys(props).map((attr) => attrs[attr]),
-    markBoxSize(),
-  ];
+  const attrProps = () => {
+    const prop = style && style.shape + style.state;
 
-  const hoverBox = (content: ReactChild) =>
-    hover && <p className={cx(hoverBoxAttr())}>{content}</p>;
+    return Object.keys(attrs)
+      .map(shape => prop === shape && attrs[shape])
+      .filter(el => el);
+  };
 
-  const show = () => {
-    const helpMeContent = (
-      <>
-        !필요해요!
-        <br />
-        나에게 없는 재능이 필요하면
-        <br />
-        도움을 요청해보세요.
-      </>
-    );
-
-    const helpUContent = (
-      <>
-        ?필요해요?
-        <br />
-        내가 가진 재능이 필요한
-        <br />
-        사람이 있는지 물어보세요.
-      </>
-    );
-
-    return (
-      <div className={cx(wrap)}>
-        <div className={cx(commonStyle, attrProps)} />
-        {helpMeAble || helpMeDisable
-          ? hoverBox(helpMeContent)
-          : hoverBox(helpUContent)}
+  return (
+    <div className={cx(wrap())}>
+      <div className={cx(markBoxSize(), commonStyle())}>
+        <Img src={attrProps()} />
       </div>
-    );
-  };
-
-  return show();
+    </div>
+  );
 }
 
 export default MarkBox;
+
+MarkBox.defaultProps = {
+  style: { shape: '!', state: 'able' },
+};

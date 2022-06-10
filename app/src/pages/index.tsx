02bd5e827748a0fc28915a06d3597f77e2aabb31
김성제 <rlatjdwp0211@naver.com>
2022-06-10 22:15:35 +0900

    Add(sungje-feature/components): page Index 추가 #10

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -5,5 +5,19 @@ import Home from './home/Home';
 import { MyPage, OtherPage } from './mypage';
 import Resign from './resign';
 import Profile from './profile';
-import Spec from './spec';
-export { HG, Home, MyPage, OtherPage, Resign, Profile, Spec };
+import SJ from './test/SJ';
+import CreatePost from './createpost/CreatePost';
+import Board from './board/Board';
+import Post from './post/Post';
+export {
+  HG,
+  SJ,
+  Home,
+  MyPage,
+  OtherPage,
+  Resign,
+  Profile,
+  CreatePost,
+  Board,
+  Post,
+};

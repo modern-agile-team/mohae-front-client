60b60980c50f578ac67606602dc3f9e0dc5669bc
김성제 <rlatjdwp0211@naver.com>
2022-06-12 19:44:58 +0900

    Add(sungje-feature/components): QuickMenu 추가 #10

---
+++ app/src/pages/post/QuickMenu.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { css, cx } from '@emotion/css';
+import PostInfo from './PostInfo';
+import PostWriter from './PostWriter';
+
+function QuickMenu() {
+  const style = css`
+    width: 1128px;
+    height: 92px;
+    background-color: white;
+    border-radius: 0px 0px 6px 6px;
+    box-shadow: 0px 4px 4px 0px #0000001a;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0px 24px 20px 24px;
+    .userData {
+      margin-right: 16px;
+    }
+  `;
+  return (
+    <div className={cx(style)}>
+      <PostInfo quickMenu />
+      <PostWriter />
+    </div>
+  );
+}
+
+export default QuickMenu;

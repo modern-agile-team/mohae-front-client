b5c6cd013877b35d4f0b52b03673d9445c7b6a81
김성제 <rlatjdwp0211@naver.com>
2022-06-12 19:37:03 +0900

    Modify(sungje-feature/components): page컴포넌트 수정 #10

--- app/src/pages/post/PostInfo.tsx
+++ app/src/pages/post/PostInfo.tsx
@@ -6,6 +6,7 @@ import { color, font, radius } from '../../styles';
 // target, 카테고리, 제목, d-day, 지역, 작성자여부, 좋아요, 조회수, 가격
 
 function PostInfo(props: Props) {
+  const { quickMenu } = props;
   const dummy = {
     statusCode: 200,
     msg: '게시글 상세 조회가 완료되었습니다.',
@@ -44,45 +45,12 @@ function PostInfo(props: Props) {
   //   };
 
   const wrap = css`
-    border-bottom: 1px solid ${color.light4};
-    width: 736px;
+    border-bottom: ${!quickMenu && ` 1px solid ${color.light4}`};
+    width: ${!quickMenu && '736px'};
     display: flex;
     justify-content: space-between;
     color: ${color.dark1};
     ${font.weight[400]}
-
-    .sectionWrap1 {
-      height: 154px;
-      display: flex;
-      justify-content: space-between;
-      flex-direction: column;
-
-      .title {
-        ${font.weight[700]}
-        ${font.size[24]}
-      display: flex;
-        align-items: center;
-
-        :after {
-          content: 'D ${dummy.response.decimalDay}';
-          width: 47px;
-          height: 24px;
-          background-color: ${color.main};
-          color: white;
-          ${font.size[14]}
-          ${font.weight[400]}
-        text-align: center;
-          padding-top: 2px;
-          margin: 2px 0px 0px 16px;
-        }
-      }
-
-      .price {
-        margin-bottom: 16px;
-        ${font.size[22]}
-        ${font.weight[700]}
-      }
-    }
 
     .sectionWrap2 {
       display: flex;
@@ -109,35 +77,113 @@ function PostInfo(props: Props) {
     }
   `;
 
-  return (
-    <div className={cx(wrap)}>
-      <div className='sectionWrap1'>
-        <div>
-          <p className='coordinates'>
-            {dummy.response.target ? '구할래요' : '해줄래요'} {'>'} 카테고리{' '}
-            {'>'} {dummy.response.categoryName}
+  const difContents = () => {
+    const common = css`
+      display: flex;
+      flex-direction: column;
+
+      .title {
+        display: flex;
+        align-items: center;
+        :after {
+          content: 'D ${dummy.response.decimalDay}';
+          width: 47px;
+          height: 24px;
+          background-color: ${color.main};
+          color: white;
+          ${font.size[14]}
+          ${font.weight[400]}
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin: 2px 0px 0px 16px;
+        }
+      }
+      .price {
+        :after {
+          content: '${dummy.response.price && '원'}';
+          margin: 0px 0px 0px 4px;
+        }
+      }
+    `;
+
+    const sectionWrap1 = () =>
+      !quickMenu
+        ? css`
+            ${common}
+            height: 154px;
+            .title {
+              ${font.weight[700]}
+              ${font.size[24]}
+            }
+            .price {
+              margin-bottom: 16px;
+              ${font.size[24]}
+              ${font.weight[700]}
+            :after {
+                ${font.size[22]}
+                ${font.weight[400]}
+              }
+            }
+          `
+        : css`
+            ${common}
+            .title {
+              ${font.weight[700]}
+              ${font.size[16]}
+            }
+            .price {
+              height: 21px;
+              ${font.size[14]}
+              ${font.weight[700]}
+            :after {
+                ${font.size[14]}
+                ${font.weight[400]}
+              }
+            }
+          `;
+
+    return !quickMenu ? (
+      <>
+        <div className={cx(sectionWrap1())}>
+          <div>
+            <p className='coordinates'>
+              {dummy.response.target ? '구할래요' : '해줄래요'} {'>'} 카테고리{' '}
+              {'>'} {dummy.response.categoryName}
+            </p>
+            <p className='title'>{dummy.response.title}</p>
+            <p className='area'>{dummy.response.areaName}</p>
+          </div>
+          <p className='price'>
+            {dummy.response.price
+              ? dummy.response.price.toLocaleString()
+              : '무료'}
           </p>
-          <p className='title'>{dummy.response.title}</p>
-          <p className='area'>{dummy.response.areaName}</p>
         </div>
+        <div className='sectionWrap2'>
+          <div className='textBtnWrap'>
+            <p>수정하기</p>
+            <p>삭제하기</p>
+          </div>
+          <div className='textBtnWrap'>
+            <p>좋아요 {dummy.response.likeCount}개</p>
+            <p>조회수 {dummy.response.hit}회</p>
+          </div>
+        </div>
+      </>
+    ) : (
+      <div className={cx(sectionWrap1())}>
+        <p className='title'>{dummy.response.title}</p>
         <p className='price'>
           {dummy.response.price
-            ? dummy.response.price.toLocaleString() + '원'
+            ? dummy.response.price.toLocaleString()
             : '무료'}
         </p>
       </div>
-      <div className='sectionWrap2'>
-        <div className='textBtnWrap'>
-          <p>수정하기</p>
-          <p>삭제하기</p>
-        </div>
-        <div className='textBtnWrap'>
-          <p>좋아요 {dummy.response.likeCount}개</p>
-          <p>조회수 {dummy.response.hit}회</p>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  };
+
+  return <div className={cx(wrap)}>{difContents()}</div>;
 }
 
 export default PostInfo;

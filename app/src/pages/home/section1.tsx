3d10f5fe79c8325f9be82086b352144d6e87e948
HanGyeol Lee <69745441+leephoter@users.noreply.github.com>
2022-06-13 13:46:45 +0900

    Update section1.tsx

--- app/src/pages/home/section1.tsx
+++ app/src/pages/home/section1.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect, useCallback, useRef } from 'react';
 import { css, cx } from '@emotion/css';
 import { color, radius, font, shadow } from '../../styles';
-import { Snap, Category, Img } from '../../components';
+import { Snap, Categories, Img } from '../../components';
 
 interface Props {
   [key: string]: any;
@@ -113,7 +113,7 @@ export default function Part1(props: Props) {
           />
         </div>
       </div>
-      <Category num={8} />
+      <Categories num={8} />
       <div
         className={cx(css`
           margin: 32px auto 0;

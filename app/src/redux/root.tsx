ca26a387b22d1690c2f609705a1329dc199d816f
leephoter <estarg51@gmail.com>
2022-06-20 00:03:12 +0900

    modify redux-root

--- app/src/redux/root.tsx
+++ app/src/redux/root.tsx
@@ -3,12 +3,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mainReducer from './main/reducer';
 import modalReducer from './modal/reducer';
+import filterReducer from './filter/reducer';
 import userReducer from './user/reducer';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     main: mainReducer,
+    filter: filterReducer,
     modal: modalReducer,
   },
 });

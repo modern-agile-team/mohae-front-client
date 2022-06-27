1df1542e584ada77d05dfb6923cac32b4bf22e65
김성제 <rlatjdwp0211@naver.com>
2022-06-27 14:01:57 +0900

    Modify(sungje/feature-sj): Board를 Container, Presenter로 분리한 거 병합

--- app/src/pages/board/Board.tsx
+++ app/src/pages/board/Board.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { css, cx } from '@emotion/css';
 import { Btn, Img, Poster, Search } from '../../components';
-import { color, font, radius } from '../../styles';
-import { Link } from 'react-router-dom';
+import { color, font } from '../../styles';
+import { Link, useParams } from 'react-router-dom';
 import Categories from '../../components/category/Categories';
+import axios from 'axios';
 
 interface PostData {
   decimalDay: number | null;
   no: number;
   title: string;
   isDeadline: number;
+  boardPhoto: string | null;
   price: number | null;
   target: number;
   areaNo: number;
@@ -17,15 +19,12 @@ interface PostData {
   userNickname: string;
 }
 
-interface Props {
-  data: {
-    boardNum: number;
-    boards: PostData[];
-    categoryName: string;
-  };
+interface Data {
+  category: { boards: PostData[] };
+  categoryName: string;
 }
 
-function Presenter({ data }: Props) {
+function Presenter() {
   const title = css`
     width: 100%;
     height: 36px;
@@ -64,6 +63,7 @@ function Presenter({ data }: Props) {
           width: 100%;
           flex-wrap: wrap;
           margin-bottom: 64px;
+          padding-left: 8px;
           padding-bottom: 16px;
         `,
       ];
@@ -80,31 +80,41 @@ function Presenter({ data }: Props) {
     `,
   };
 
+  const [data, setData] = useState<Data>();
+  const { no } = useParams();
+
+  const getData = () => {
+    axios
+      .get(`https://mo-hae.site/boards/category/${no}?take=12&page=1`)
+      .then(res => {
+        setData(res.data.response);
+        console.log('res.data.response :>> ', res.data.response);
+      })
+      .catch(err => console.log('err', err));
+  };
+
+  useEffect(() => {
+    getData();
+  }, [no]);
+
   const createPost = () => {
     const gap = (i: number) => css`
       margin-top: 24px;
-      margin-right: ${i % 4 && '24px'};
+      margin-right: ${i % 4 && '16px'};
     `;
-    return data.boards.map((el, i) => (
-      <Link key={i} className={cx(gap(i + 1))} to={`/post/${el.no}`}>
-        <Poster data={data.boards[i]} />
-      </Link>
-    ));
+    return (
+      data &&
+      data.category.boards.map((el: any, i: any) => (
+        <Link key={i} className={cx(gap(i + 1))} to={`/post/${el.no}`}>
+          <Poster data={data.category.boards[i]} />
+        </Link>
+      ))
+    );
   };
-
-  // useEffect(() => {
-  //   window.addEventListener('scroll', e => {
-  //     const scrollY = Math.round(window.scrollY);
-  //     scrollY / 1250 === 0 && console.log(scrollY);
-  //   });
-  //   스크롤 정도에 따라 게시물 요청 후 setPost 한 후 createPost()
-  // });
-
-  //1250
 
   return (
     <>
-      <div className={cx(title)}>{data.categoryName}</div>
+      <div className={cx(title)}>{data?.categoryName}</div>
       <Categories num={7} />
       <div className={cx(style.wrap(0))}>
         <Search board />
@@ -120,7 +130,8 @@ function Presenter({ data }: Props) {
         </div>
       </div>
       <div className={cx(style.wrap(1))}>
-        총&nbsp;<p>{data.boards.length.toLocaleString()}</p>&nbsp;건의 게시물
+        총&nbsp;<p>{data?.category.boards.length}</p>
+        &nbsp;건의 게시물
       </div>
       <div className={cx(style.wrap(2))}>{createPost()}</div>
     </>

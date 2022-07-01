d3af7f471393edbb38115438ee2f8b48311bf1e5
leephoter <estarg51@gmail.com>
2022-07-01 10:57:20 +0900

    refresh to build

--- app/src/pages/test/HG.tsx
+++ app/src/pages/test/HG.tsx
@@ -1,48 +1,29 @@
 /** @format */
 
-import { useState, useEffect } from 'react';
-import { injectGlobal, cx, css } from '@emotion/css';
-import {
-  Categories,
-  Category,
-  OrderedImg,
-  MarkBox,
-  Img,
-  NewPost,
-  Carousel,
-} from '../../components';
-import Edit from '../spec/Edit';
-import Spec from '../spec';
-import Visit from '../spec/Visit';
+// import { useState, useEffect } from 'react';
+// import { injectGlobal, cx, css } from '@emotion/css';
+// import {
+//   Categories,
+//   Category,
+//   OrderedImg,
+//   MarkBox,
+//   Img,
+//   NewPost,
+//   Carousel,
+// } from '../../components';
+// import Edit from '../spec/Edit';
+// import Spec from '../spec';
+// import Visit from '../spec/Visit';
 
-import { RootState, AppDispatch } from '../../redux/root';
-import { Profile, LoginModal } from '../../pages';
-import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import { useForm } from 'react-hook-form';
-import TestImg from './test';
-import OtherPage from '../mypage/mypage/OtherPage';
-import { spec_create } from '../../redux/modal/reducer';
-
-interface BODY {
-  title: string;
-  description: string;
-  image: FormData;
-}
+// import { RootState, AppDispatch } from '../../redux/root';
+// import { Profile, LoginModal } from '../../pages';
+// import axios from 'axios';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { useForm } from 'react-hook-form';
+// import TestImg from './test';
+// import OtherPage from '../mypage/mypage/OtherPage';
+// import { spec_create } from '../../redux/modal/reducer';
 
 export default function HG() {
-  const isOpen = useSelector((state: RootState) => state.modal.openSpecCreate);
-  const dispatch = useDispatch();
-  const openModal = () => {
-    dispatch(spec_create(!isOpen));
-  };
-
-  return (
-    <>
-      {/* <Spec /> */}
-      {/* <button onClick={openModal}>BUTTON</button> */}
-
-      {/* <Visit /> */}
-    </>
-  );
+  return <div>1</div>;
 }

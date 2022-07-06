588cb880d9bf7622f01582b0be3dc4c15b6bd1a2
김성제 <rlatjdwp0211@naver.com>
2022-07-06 16:30:20 +0900

    Add(sungje/feature-sj): board & filter reducer 생성

--- app/src/redux/filter/reducer.tsx
+++ app/src/redux/filter/reducer.tsx
@@ -6,30 +6,49 @@ import {
 } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-// export const checked = createSlice(
-//   JSON.parse(
-//     localStorage.getItem('filterChecked') ||
-//       JSON.stringify({
-//         정렬: { 0: false, 1: true, 2: false },
-//         대상: { 0: false, 1: false },
-//         기간: { 0: false, 1: false, 2: false, 3: false },
-//         무료: { 0: false },
-//       })
-//   )
-// );
-
-const initialState = {
-  정렬: { 0: false, 1: true, 2: false },
-  대상: { 0: false, 1: false },
-  기간: { 0: false, 1: false, 2: false, 3: false },
-  무료: { 0: false },
+const initialState: {
+  data: {
+    check: {
+      sort: { [key: string]: boolean };
+      target: { [key: string]: boolean };
+      date: { [key: string]: boolean };
+      free: { [key: string]: boolean };
+    };
+    area: {
+      areaName: string;
+      areaNo: string;
+    };
+  };
+} = {
+  data: {
+    check: {
+      sort: { 0: false, 1: true, 2: false },
+      target: { 0: false, 1: false },
+      date: { 0: false, 1: false, 2: false, 3: false },
+      free: { 0: false },
+    },
+    area: {
+      areaName: '전체 지역',
+      areaNo: '0',
+    },
+  },
 };
 
 const filter = createSlice({
-  name: 'msgbox',
+  name: 'filter',
   initialState,
-  reducers: {},
+  reducers: {
+    setCheck: (state, action: PayloadAction<any>) => {
+      state.data.check = action.payload;
+    },
+    setAreaName: (state, action: PayloadAction<any>) => {
+      state.data.area.areaName = action.payload;
+    },
+    setAreaNo: (state, action: PayloadAction<any>) => {
+      state.data.area.areaNo = action.payload;
+    },
+  },
 });
 
-export const {} = filter.actions;
+export const { setCheck, setAreaName, setAreaNo } = filter.actions;
 export default filter.reducer;

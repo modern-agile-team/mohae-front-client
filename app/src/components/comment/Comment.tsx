69e17d0f816b8dabd8964bb0afa618eaefc741be
민순기 <skmin@rsupport.com>
2022-07-11 14:48:21 +0900

    [Feat]: Comment Component 생성 #116

---
+++ app/src/components/comment/Comment.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+const Comment = () => {
+  return <Wrapper>Comment Wrapper</Wrapper>;
+};
+
+export default Comment;
+
+const Wrapper = styled.div`
+  box-shadow: 0px 0px 8px rgba(132, 131, 141, 0.5);
+  border-radius: 6px;
+  padding: 16px 24px;
+`;

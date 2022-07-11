1eb62420029b444c5b968266cca89da8890c7efc
민순기 <skmin@rsupport.com>
2022-07-11 15:02:25 +0900

    [Feat]: Input 창 UI 적용 #116

--- app/src/components/comment/CommentInputForm.tsx
+++ app/src/components/comment/CommentInputForm.tsx
@@ -1,15 +1,38 @@
+import styled from '@emotion/styled';
+
 const CommentInputForm = () => {
   return (
-    <section>
-      <div>
+    <Wrapper>
+      <CommentCounter>
         <p>댓글 (5)</p>
-        <form>
-          <input type="text" />
-          <button type="submit">작성</button>
-        </form>
-      </div>
-    </section>
+      </CommentCounter>
+      <FormContainer>
+        <input type="text" placeholder="댓글을 입력해 주세요. (최대 500자)" />
+        <button type="submit">작성</button>
+      </FormContainer>
+    </Wrapper>
   );
 };
 
 export default CommentInputForm;
+
+const Wrapper = styled.section``;
+
+const CommentCounter = styled.div`
+  border-bottom: 1px solid #e7e7e8;
+  p {
+    padding-bottom: 16px;
+  }
+`;
+
+const FormContainer = styled.form`
+  padding-top: 16px;
+  input {
+    width: 100%;
+    padding: 16px;
+    background: #ffffff;
+    box-shadow: inset 0px 0px 8px rgba(132, 131, 141, 0.2);
+    border-radius: 6px;
+    min-height: 100px;
+  }
+`;

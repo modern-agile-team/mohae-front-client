4df5dd3c541458052d93fcdb26e3ed9b6445ad5d
김성제 <rlatjdwp0211@naver.com>
2022-08-08 17:30:43 +0900

    [Modify] edit일 경우 post data get요청해서 create/reducer에 할당 #145

--- app/src/pages/createpost/Container.tsx
+++ app/src/pages/createpost/Container.tsx
@@ -1,15 +1,22 @@
 import axios from 'axios';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import EmptySpinner from '../../components/emptySpinner/EmptySpinner';
+import useRefactorPostingData from '../../customhook/useRefactorPostingData';
 import { setForEdit, setLoading } from '../../redux/createpost/reducer';
+import { setInitialState } from '../../redux/post/reducer';
 import { RootState } from '../../redux/root';
+import { ENDPOINT } from '../../utils/ENDPOINT';
 import getToken from '../../utils/getToken';
 import Presenter from './Presenter';
 
 interface Props {
   type: string;
+}
+
+interface StateForEdit {
+  [key: string]: string | number | null | string[] | never[];
 }
 
 function CreateAndEditPost({ type }: Props) {
@@ -18,86 +25,86 @@ function CreateAndEditPost({ type }: Props) {
   const loadingState = useSelector(
     (state: RootState) => state.createPost.loading,
   );
-  const reduxData: { [key: string]: any } = useSelector(
-    (state: RootState) => state.createPost.data,
-  );
-  const postingData = useSelector(
-    (state: RootState) => state.post.data.response.board,
-  );
-  const dataBeforeeEdit: { [key: string]: any } = {
-    price: String(Number(postingData.price).toLocaleString()),
-    title: postingData.title,
-    description: postingData.description,
-    summary: postingData.summary === null ? '' : postingData.summary,
-    target: postingData.target,
-    categoryNo: postingData.categoryNo,
-    areaNo: postingData.areaNo,
-    deadline: null,
-    imgArr:
-      postingData.boardPhotoUrls !== null && postingData.boardPhotoUrls !== ''
-        ? postingData.boardPhotoUrls.split(', ').map(el => {
-            return 'https://d2ffbnf2hpheay.cloudfront.net/' + el;
-          })
-        : [],
-  };
+  const form = useSelector((state: RootState) => state.createPost.form);
+  const refactorReduxData = useRefactorPostingData();
+  const [stateForEdit, setStateForEdit] = useState<StateForEdit>();
+  const [view, setView] = useState<{ [key: number]: boolean }>({
+    0: false,
+    1: false,
+    2: false,
+  });
+  const [targetChecked, setTargetChecked] = useState<{
+    [key: number]: boolean;
+  }>({ 0: true, 1: false });
 
   useEffect(() => {
     if (type === 'edit') {
-      dispatch(setForEdit(dataBeforeeEdit));
+      axios
+        .get(`${ENDPOINT}boards/${no}`, {
+          headers: {
+            Authorization: `Bearer ${getToken()}`,
+          },
+        })
+        .then(res => {
+          const data = res.data.response.board;
+          const beforeEdit = {
+            price: String(Number(data.price).toLocaleString()),
+            title: data.title,
+            description: data.description,
+            summary: data.summary === null ? '' : data.summary,
+            target: data.target,
+            categoryNo: data.categoryNo,
+            areaNo: data.areaNo,
+            deadline: null,
+            imgArr:
+              data.boardPhotoUrls !== null && data.boardPhotoUrls !== ''
+                ? data.boardPhotoUrls.split(', ').map((el: any) => {
+                    return 'https://d2ffbnf2hpheay.cloudfront.net/' + el;
+                  })
+                : [],
+          };
+          setStateForEdit(beforeEdit);
+          dispatch(setForEdit(beforeEdit));
+          setTargetChecked(
+            Number(beforeEdit.target) === 1
+              ? { 0: false, 1: true }
+              : { 0: true, 1: false },
+          );
+        })
+        .catch(err => console.log('err', err));
     } else dispatch(setLoading(false));
   }, []);
-  const form = useSelector((state: RootState) => state.createPost.form);
 
-  const refactorPriceData = useMemo((): number => {
-    const newData = Number(reduxData.price.replace(/,/g, ''));
-    return newData;
-  }, [reduxData.price]);
-  const refactorCategoryNo = useMemo((): number => {
-    const newData = Number(reduxData.categoryNo);
-    return newData;
-  }, [reduxData.categoryNo]);
-  const refactorAreaNo = useMemo((): number => {
-    const newData = Number(reduxData.areaNo);
-    return newData;
-  }, [reduxData.areaNo]);
-  const refactorDeadline = useMemo((): number => {
-    const newData = Number(reduxData.deadline);
-    return newData;
-  }, [reduxData.deadline]);
-  const refactorSummary = useMemo((): string | null => {
-    const newData = reduxData.summary === '' ? null : reduxData.summary;
-    return newData;
-  }, [reduxData.summary]);
-  const refactorDescription = useMemo((): string => {
-    const newData = reduxData.description;
-    return newData;
-  }, [reduxData.description]);
-  const refactorTitle = useMemo((): string => {
-    const newData = reduxData.title;
-    return newData;
-  }, [reduxData.title]);
+  const requestAxios = (type: string) => {
+    const URL =
+      type === 'create'
+        ? `https://mo-hae.site/boards`
+        : `https://mo-hae.site/boards/${no}`;
+    const config = {
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${getToken()}`,
+      },
+    };
+    const axiosPostOrPatch = type === 'create' ? axios.post : axios.patch;
 
-  const refactorReduxData: { [key: string]: string | number | null } = {
-    price: refactorPriceData,
-    title: refactorTitle,
-    description: refactorDescription,
-    summary: refactorSummary,
-    target: reduxData.target,
-    categoryNo: refactorCategoryNo,
-    areaNo: refactorAreaNo,
-    deadline: refactorDeadline,
+    axiosPostOrPatch(URL, form, config)
+      .then(res => {
+        dispatch(setInitialState());
+      })
+      .catch(err => {
+        type === 'create' ? alert('작성 실패') : alert('수정 실패');
+      });
   };
-
-  console.log('dataBeforeeEdit :>> ', dataBeforeeEdit);
-  console.log('refactorReduxData :>> ', refactorReduxData);
 
   const postingAxios = (e: React.MouseEvent, type: string) => {
     e.preventDefault();
 
     if (type === 'edit') {
-      for (const key in dataBeforeeEdit) {
+      for (const key in stateForEdit) {
         if (key !== 'imgArr') {
-          refactorReduxData[key] !== dataBeforeeEdit[key]
+          refactorReduxData[key] !== stateForEdit[key]
             ? form.set(`${key}`, JSON.stringify(refactorReduxData[key]))
             : form.set(`${key}`, JSON.stringify(null));
         }
@@ -109,69 +116,13 @@ function CreateAndEditPost({ type }: Props) {
     }
 
     if (form.getAll('image').length === 0) {
-      const file = new File(['logo.jpg'], 'logo.jpg', {
+      const file = new File(['logo.png'], 'logo.png', {
         type: 'image/jpg',
       });
       form.append('image', file);
     }
-
-    console.log('image :>> ', form.getAll('image'));
-    console.log('price :>> ', form.getAll('price'));
-    console.log('title :>> ', form.getAll('title'));
-    console.log('description :>> ', form.getAll('description'));
-    console.log('summary :>> ', form.getAll('summary'));
-    console.log('target :>> ', form.getAll('target'));
-    console.log('categoryNo :>> ', form.getAll('categoryNo'));
-    console.log('areaNo :>> ', form.getAll('areaNo'));
-    console.log('deadline :>> ', form.getAll('deadline'));
-
-    type === 'create'
-      ? axios
-          .post(`https://mo-hae.site/boards`, form, {
-            headers: {
-              accept: 'application/json',
-              'Content-Type': 'multipart/form-data',
-              Authorization: `Bearer ${getToken()}`,
-            },
-          })
-          .then(res => res)
-          .catch(err => {
-            console.log('err.data :>> ', err);
-            type === 'create' ? alert('작성 실패') : alert('수정 실패');
-          })
-      : axios
-          .patch(`https://mo-hae.site/boards/${no}`, form, {
-            headers: {
-              accept: 'application/json',
-              'Content-Type': 'multipart/form-data',
-              Authorization: `Bearer ${getToken()}`,
-            },
-          })
-          .then(res => res)
-          .catch(err => {
-            console.log('err.data :>> ', err);
-            type === 'create' ? alert('작성 실패') : alert('수정 실패');
-          });
+    requestAxios(type);
   };
-
-  const [view, setView] = useState<{ [key: number]: boolean }>({
-    0: false,
-    1: false,
-    2: false,
-  });
-  const [targetChecked, setTargetChecked] = useState<{
-    [key: number]: boolean;
-  }>(
-    reduxData.target
-      ? {
-          0: false,
-          1: true,
-        }
-      : {
-          0: true,
-          1: false,
-        },
-  );
 
   const selectBoxClick = (i: number) => {
     setView({ 0: false, 1: false, 2: false, [i]: !view[i] });
@@ -205,3 +156,12 @@ function CreateAndEditPost({ type }: Props) {
 }
 
 export default CreateAndEditPost;
+
+// console.log('price :>> ', form.getAll('price'));
+// console.log('title :>> ', form.getAll('title'));
+// console.log('description :>> ', form.getAll('description'));
+// console.log('target :>> ', form.getAll('target'));
+// console.log('categoryNo :>> ', form.getAll('categoryNo'));
+// console.log('deadline :>> ', form.getAll('deadline'));
+// console.log('areaNo :>> ', form.getAll('areaNo'));
+// console.log('image :>> ', form.getAll('image'));

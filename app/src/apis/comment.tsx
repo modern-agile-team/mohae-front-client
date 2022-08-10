ad27104f96db1a3a38458413f58341df22a5b805
김성제 <rlatjdwp0211@naver.com>
2022-08-10 17:01:01 +0900

    [Add] 댓글 관련 API 추가 #151

--- app/src/apis/comment.tsx
+++ app/src/apis/comment.tsx
@@ -1,15 +1,32 @@
 import instance from './instance';
 
+interface postCommentType {
+  no: number;
+  body?: {
+    content: string;
+  };
+}
+
+interface EditDeleteComment extends postCommentType {
+  commentNo: number;
+}
+
 export const getCommentList = (no: number) => {
   return instance.get(`/board/${no}/comments`);
 };
 
-type postCommentType = {
-  no: number;
-  body: {
-    content: string;
-  };
-};
 export const createComment = ({ no, body }: postCommentType) => {
   return instance.post(`/board/${no}/comments`, body);
+};
+
+export const editComment = (
+  no: number,
+  commentNo: number,
+  body: EditDeleteComment,
+) => {
+  return instance.put(`/board/${no}/comments/${commentNo}`, body);
+};
+
+export const deleteComment = ({ no, commentNo }: EditDeleteComment) => {
+  return instance.put(`/board/${no}/comments/${commentNo}`);
 };

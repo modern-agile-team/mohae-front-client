6d8f974509a2577944af22f6c38a76f8775574f5
baebumsoo <baebeomsu77@gmail.com>
2022-09-24 17:33:29 +0900

    Add(feauture/bumsoo/#135): 프로필 수정 시 닉네임 중복체크 기능 구현

--- app/src/utils/loginCheck.ts
+++ app/src/utils/loginCheck.ts
@@ -4,7 +4,7 @@ import { getUserData } from '../redux/user/reducer';
 
 export const loginCheck = () => {
   const data = decodingToken();
-
+  
   if (data) {
     try {
       store.dispatch(getUserData(data?.userNo));

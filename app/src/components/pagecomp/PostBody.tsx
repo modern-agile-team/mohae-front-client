4c54559f8a6175168e860eb27f267ed4e69cabe5
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-06 21:38:37 +0900

    [Refactor] TextEditor Component 파일 위치 변경 #243

--- app/src/components/pagecomp/PostBody.tsx
+++ app/src/components/pagecomp/PostBody.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { css, cx } from '@emotion/css';
 import { Box, TextEditor } from '..';
-import { font } from '../../styles';
 import { Props } from '../../pages/post/Container';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/root';
@@ -14,34 +13,8 @@ interface PostBodyProps {
   type?: string;
 }
 function PostBody(props: PostBodyProps) {
-  const { view, getValue, type } = props;
+  const { view } = props;
   const reduxData = useSelector((state: RootState) => state.post.data.response);
-
-  const style = css`
-    margin: ${view ? '32px 0px 16px 0px' : '32px 0px 64px 0px'};
-    .body {
-      padding: ${view && '16px 0px 16px 24px'};
-      ${font.size[14]}
-      ${font.weight[400]}
-      word-break: break-all;
-      .rdw-editor-main {
-        height: 349px !important;
-        padding: 0px !important;
-      }
-      .DraftEditor-editorContainer {
-        padding: 6px;
-      }
-    }
-
-    .description-scrollBox {
-      width: 1080px;
-      height: 365px;
-      overflow-y: auto;
-    }
-    .description-box {
-      height: fit-content;
-    }
-  `;
 
   const body = () => {
     return view ? (
@@ -62,7 +35,7 @@ function PostBody(props: PostBodyProps) {
   };
 
   return (
-    <div className={cx(style)}>
+    <div className={cx(style(view))}>
       <Box size={[1128, 397]} className="body">
         {body()}
       </Box>
@@ -71,3 +44,29 @@ function PostBody(props: PostBodyProps) {
 }
 
 export default PostBody;
+
+const style = (view?: boolean) => css`
+  margin: ${view ? '32px 0px 16px 0px' : '32px 0px 64px 0px'};
+  .body {
+    padding: ${view && '16px 0px 16px 24px'};
+    font-size: 14px;
+    font-family: 'Regular';
+    word-break: break-all;
+    .rdw-editor-main {
+      height: 349px !important;
+      padding: 0px !important;
+    }
+    .DraftEditor-editorContainer {
+      padding: 6px;
+    }
+  }
+
+  .description-scrollBox {
+    width: 1080px;
+    height: 365px;
+    overflow-y: auto;
+  }
+  .description-box {
+    height: fit-content;
+  }
+`;

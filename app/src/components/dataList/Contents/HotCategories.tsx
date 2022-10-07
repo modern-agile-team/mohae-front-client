1fb3a4ff662e9a57332659cdd690a98fade917d8
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-07 20:55:40 +0900

    [Refactor] hHotCategories 컴포넌트 css 수정 #250

--- app/src/components/dataList/Contents/HotCategories.tsx
+++ app/src/components/dataList/Contents/HotCategories.tsx
@@ -1,7 +1,7 @@
-import { css, cx } from '@emotion/css';
 import React, { useEffect, useState } from 'react';
+import styled from '@emotion/styled';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
-import requestHotCategories from '../../../apis/hotCategories';
+import getHotCategories from '../../../apis/hotCategories';
 import {
   HotCategoriesProps,
   HotCategoriesType,
@@ -17,8 +17,24 @@ function HotCategories(props: HotCategoriesProps) {
   );
   const [hotCategories, setHotCategories] = useState<HotCategoriesType[]>([]);
 
+  const requestHotCategories = async () => {
+    try {
+      await getHotCategories().then(res => {
+        const addRanking: HotCategoriesType[] = Object.keys(
+          res.data.response,
+        ).map(el => {
+          return { ...res.data.response[el], ranking: Number(el) + 1 };
+        });
+
+        setHotCategories([...addRanking]);
+      });
+    } catch (err: any) {
+      alert(err.response.data.error.message);
+    }
+  };
+
   useEffect(() => {
-    requestHotCategories(setHotCategories);
+    requestHotCategories();
   }, []);
 
   const handleHotCategoriesClick = (e: React.MouseEvent, no: number) => {
@@ -39,57 +55,59 @@ function HotCategories(props: HotCategoriesProps) {
   };
 
   return (
-    <div className={cx(hotCategoriesWrap)}>
+    <Container>
       {children}
       {hotCategories.map((el, _) => (
-        <div
+        <HotCategorieWrapper
           key={el.no}
-          className={cx(hotCategoriestyle)}
           onClick={e => handleHotCategoriesClick(e, el.no)}
         >
-          <div id="no">{el.ranking}</div>
-          <div id="categoryName">{el.name}</div>
-        </div>
+          <RankingNumber>{el.ranking}</RankingNumber>
+          <CategoryName>{el.name}</CategoryName>
+        </HotCategorieWrapper>
       ))}
-    </div>
+    </Container>
   );
 }
 export default HotCategories;
 
-const hotCategoriesWrap = css`
+const Container = styled.section`
   padding-left: 24px;
   color: #4f4e5c;
   border-left: 1px solid #e7e7e8;
-  p {
+  p,
+  strong {
     display: flex;
+    align-items: center;
     height: 32px;
     margin-bottom: 8px;
   }
 `;
 
-const hotCategoriestyle = css`
+const CategoryName = styled.p`
+  width: 325px;
+  height: 24px;
+  padding-top: 1px;
+  font-size: 14px;
+  font-family: 'Regular';
+`;
+
+const HotCategorieWrapper = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 8px;
   height: 24px;
   cursor: pointer;
-  &:hover #categoryName {
+  &:hover p {
     font-family: 'Bold';
   }
-  #no {
-    width: 20px;
-    height: 20px;
-    text-align: center;
-    color: #ff445e;
-    font-size: 12px;
-    font-family: 'Bold';
-    margin: 2px 8px 0px 0px;
-  }
-  #categoryName {
-    width: 325px;
-    height: 24px;
-    padding-top: 1px;
-    font-size: 14px;
-    font-family: 'Regular';
-  }
+`;
+
+const RankingNumber = styled.strong`
+  width: 20px;
+  text-align: center;
+  color: #ff445e;
+  font-size: 12px;
+  font-family: 'Bold';
+  margin: 2px 8px 0px 0px;
 `;

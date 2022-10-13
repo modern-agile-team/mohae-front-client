d1fd047e78f7691a7d55050c806803f8ebd14178
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-13 18:05:24 +0900

    [Delete] 파일명 변경 또는 파일 이름 변경으로 인한 Delete #244

--- app/src/apis/hotCategories.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosResponse } from 'axios';
-import { customAxios } from './instance';
-
-const getHotCategories = (): Promise<AxiosResponse> => {
-  return customAxios.get(`/categories/popular`);
-};
-export default getHotCategories;

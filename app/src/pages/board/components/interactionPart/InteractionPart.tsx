c414db751099e31baf66b7562faa4f16d06ce9bd
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-19 11:20:27 +0900

    [Refactor] board/Presenter에서 분리된 board의 상단 #245

---
+++ app/src/pages/board/components/interactionPart/InteractionPart.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import {
+  Categories,
+  Img,
+  MainButton,
+  Popup,
+  Search,
+} from '../../../../components';
+import { categoryList } from '../../../../components/category/categoryList';
+import { useNavigate, useParams } from 'react-router-dom';
+import getToken from '../../../../utils/getToken';
+
+interface InteractionPartProps {
+  resetPageInfo: () => void;
+}
+
+function InteractionPart(props: InteractionPartProps) {
+  const { resetPageInfo } = props;
+  const navigation = useNavigate();
+  const { no } = useParams();
+  const categoryName = categoryList[Number(no) - 1].name;
+  const localUserToken = getToken();
+  const [loginPlz, setLoginPlz] = useState(false);
+
+  const controlWriteButton = () => {
+    if (localUserToken && localUserToken !== null) {
+      navigation('/create/post');
+    } else setLoginPlz(prev => !prev);
+  };
+
+  return (
+    <>
+      <CategoryBoard>{`${categoryName} 게시판`}</CategoryBoard>
+      <Categories num={7} resetPageInfo={resetPageInfo} />
+      <FlexWrapper>
+        <Search board resetPageInfo={resetPageInfo} />
+        <ButtonWrapper>
+          <MainButton type="button" able={true} onClick={controlWriteButton}>
+            <p>글쓰기</p>
+            <div className="imgWrap">
+              <Img src="/img/write.png" />
+            </div>
+          </MainButton>
+        </ButtonWrapper>
+      </FlexWrapper>
+      <Popup
+        visible={loginPlz}
+        text1={'게시글 작성은 로그인 후 이용 가능합니다.'}
+        overlay={controlWriteButton}
+      >
+        <PopupButtonWrapper>
+          <MainButton
+            type="button"
+            able={true}
+            onClick={() => controlWriteButton}
+          >
+            닫기
+          </MainButton>
+        </PopupButtonWrapper>
+      </Popup>
+    </>
+  );
+}
+
+export default InteractionPart;
+
+const CategoryBoard = styled.p`
+  width: 100%;
+  height: 36px;
+  font-size: 28px;
+  font-family: 'Bold';
+  color: #4f4e5c;
+  display: flex;
+  justify-content: center;
+  margin: 40px 0px 48px 0px;
+`;
+
+const FlexWrapper = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 936px;
+  margin: 64px auto 0px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 100px;
+  height: 43px;
+  .imgWrap {
+    width: 15px;
+    height: 15px;
+  }
+`;
+
+const PopupButtonWrapper = styled.div`
+  width: 74px;
+  height: 43px;
+`;

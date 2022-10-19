88d1842ac63e1c9a0bc8d0f41174e2e148dba182
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-19 11:23:25 +0900

    [Modify] 카테고리 별 조회 요청 시 디바운싱 적용 후 코드 리펙터링 #245

--- app/src/pages/board/Container.tsx
+++ app/src/pages/board/Container.tsx
@@ -1,37 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import {
-  useLocation,
-  useNavigate,
-  useParams,
-  useSearchParams,
-} from 'react-router-dom';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
+import { useLocation, useParams, useSearchParams } from 'react-router-dom';
 import {
   setResCategorys,
   setResArrEmpty,
   setResFiltering,
 } from '../../redux/board/reducer';
+import { useDispatch } from 'react-redux';
 import Presenter from './Presenter';
-import getToken from '../../utils/getToken';
 import { customAxios } from '../../apis/instance';
-
-export interface PostData {
-  decimalDay: number | null;
-  no: number;
-  title: string;
-  isDeadline: number;
-  photoUrl: string | null;
-  price: number | null;
-  target: number;
-  areaNo: number;
-  areaName: string;
-  nickname: string;
-}
-
-export interface Data {
-  response: PostData[];
-}
 
 interface PageInfo {
   category: {
@@ -49,14 +25,11 @@ function Container() {
   const { no } = useParams();
   const [searchParams, _] = useSearchParams();
   const location = useLocation();
-  const loginState = getToken();
-  const [loginPlz, setLoginPlz] = useState(false);
   const [target, setTarget] = useState<Element | null>(null);
   const [urlInfo, setUrlInfo] = useState({
     query: location.search,
     no: no,
   });
-  const navigation = useNavigate();
   const [pageInfo, setPageInfo] = useState<PageInfo>({
     category: {
       page: 1,
@@ -67,6 +40,89 @@ function Container() {
       totalPage: 1,
     },
   });
+
+  const getParams = (key: string): any => {
+    if (key === 'popular') {
+      return searchParams.get('popular') === null
+        ? `&sort=${searchParams.get('sort')}`
+        : `&${key}=${searchParams.get(key)}`;
+    }
+    if (key === 'areaNo') {
+      return searchParams.get('areaNo') === '0'
+        ? `&${key}=null`
+        : `&${key}=${searchParams.get(key)}`;
+    } else {
+      return key === 'title'
+        ? `&${key}=${decodeURIComponent(String(searchParams.get(key)))}`
+        : `&${key}=${searchParams.get(key)}`;
+    }
+  };
+
+  const filteringQuery = useCallback(() => {
+    const queryString = `${getParams('categoryNo')}${decodeURIComponent(
+      getParams('title'),
+    )}${getParams('target')}${getParams('date')}${getParams('free')}${getParams(
+      'min',
+    )}${getParams('max')}${getParams('areaNo')}${getParams('popular')}`;
+    return queryString;
+  }, [location.search]);
+
+  const getData = async () => {
+    const filteringBaseURL = `https://mo-hae.site/boards/filter?take=8&page=${pageInfo.filtering.page}`;
+    const categoryBaseURL = `https://mo-hae.site/boards/category/${no}?take=8&page=${pageInfo.category.page}`;
+    try {
+      await customAxios
+        .get(
+          location.search
+            ? filteringBaseURL + filteringQuery()
+            : categoryBaseURL,
+        )
+        .then(res => {
+          if (!location.search) {
+            dispatch(setResCategorys(res.data.response));
+            setPageInfo((prev: PageInfo) => ({
+              category: {
+                ...prev.category,
+                totalPage: pageInfo.category.totalPage + 1,
+              },
+              filtering: { page: 1, totalPage: 1 },
+            }));
+          } else {
+            dispatch(setResFiltering(res.data.response));
+            setPageInfo((prev: PageInfo) => ({
+              category: { page: 1, totalPage: 1 },
+              filtering: {
+                ...prev.filtering,
+                totalPage: pageInfo.filtering.totalPage + 1,
+              },
+            }));
+          }
+        });
+    } catch (err) {
+      alert('알 수 없는 에러가 발생하였습니다.');
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      dispatch(setResArrEmpty());
+    };
+  }, []);
+
+  useEffect(() => {
+    const deboucing = setTimeout(() => {
+      if (urlInfo.no === no && urlInfo.query === location.search) {
+        setPageInfo(prev => prev);
+        getData();
+      } else {
+        dispatch(setResArrEmpty());
+        getData();
+        setUrlInfo({ query: location.search, no: no });
+      }
+    }, 500);
+    return () => clearTimeout(deboucing);
+  }, [location.search, no, pageInfo.category.page, pageInfo.filtering.page]);
+
   const handleIntersect = useCallback(
     ([entry]: IntersectionObserverEntry[]) => {
       if (entry.isIntersecting) {
@@ -90,59 +146,15 @@ function Container() {
     [],
   );
 
-  const controlWriteButton = () => {
-    if (loginState && loginState !== null) {
-      navigation('/create/post');
-    } else setLoginPlz(true);
-  };
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersect, {
+      threshold: 0,
+      root: null,
+    });
 
-  const getParams = (query: string): any => {
-    return searchParams.get(query);
-  };
-
-  const filteringQuery = () => {
-    const queryBase = `&categoryNo=${no}&title=${decodeURIComponent(
-      getParams('title'),
-    )}&target=${getParams('target')}&date=${getParams('date')}&free=${getParams(
-      'free',
-    )}&min=${getParams('min')}&max=${getParams('max')}&areaNo=${
-      getParams('areaNo') !== '0' ? getParams('areaNo') : null
-    }`;
-    return getParams('popular') !== null
-      ? queryBase + `&popular=${getParams('popular')}`
-      : queryBase + `&sort=${getParams('sort')}`;
-  };
-
-  const getData = () => {
-    const filteringBaseURL = `https://mo-hae.site/boards/filter?take=8&page=${pageInfo.filtering.page}`;
-    const categoryBaseURL = `https://mo-hae.site/boards/category/${no}?take=8&page=${pageInfo.category.page}`;
-    customAxios
-      .get(
-        location.search ? filteringBaseURL + filteringQuery() : categoryBaseURL,
-      )
-      .then(res => {
-        if (!location.search) {
-          dispatch(setResCategorys(res.data.response));
-          setPageInfo((prev: PageInfo) => ({
-            category: {
-              ...prev.category,
-              totalPage: pageInfo.category.totalPage + 1,
-            },
-            filtering: { page: 1, totalPage: 1 },
-          }));
-        } else {
-          dispatch(setResFiltering(res.data.response));
-          setPageInfo((prev: PageInfo) => ({
-            category: { page: 1, totalPage: 1 },
-            filtering: {
-              ...prev.filtering,
-              totalPage: pageInfo.filtering.totalPage + 1,
-            },
-          }));
-        }
-      })
-      .catch(err => console.log('err', err));
-  };
+    target && observer.observe(target);
+    return () => observer.disconnect();
+  }, [handleIntersect, target]);
 
   const resetPageInfo = () => {
     setPageInfo({
@@ -151,65 +163,7 @@ function Container() {
     });
   };
 
-  useEffect(() => {
-    return () => {
-      dispatch(setResArrEmpty());
-    };
-  }, []);
-
-  useEffect(() => {
-    if (urlInfo.no === no && urlInfo.query === location.search) {
-      setPageInfo(prev => prev);
-      getData();
-    } else {
-      dispatch(setResArrEmpty());
-      getData();
-      setUrlInfo({ query: location.search, no: no });
-    }
-  }, [location.search, no, pageInfo.category.page, pageInfo.filtering.page]);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersect, {
-      threshold: 0,
-      root: null,
-    });
-
-    target && observer.observe(target);
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [handleIntersect, target]);
-
-  return (
-    <Presenter
-      resetPageInfo={resetPageInfo}
-      setTarget={setTarget}
-      categories={categories}
-      controlWriteButton={controlWriteButton}
-      loginPlz={loginPlz}
-      setLoginPlz={setLoginPlz}
-    />
-  );
+  return <Presenter resetPageInfo={resetPageInfo} setTarget={setTarget} />;
 }
 
 export default Container;
-
-const categories = [
-  { no: '1', name: '전체' },
-  { no: '2', name: '디자인' },
-  { no: '3', name: 'IT/개발' },
-  { no: '4', name: '사진/영상' },
-  { no: '5', name: '기획/마케팅' },
-  { no: '6', name: '번역/통역' },
-  { no: '7', name: '문서작업' },
-  { no: '8', name: '컨설팅' },
-  { no: '9', name: '법률' },
-  { no: '10', name: '과외/레슨' },
-  { no: '11', name: '상담/운세' },
-  { no: '12', name: '이벤트' },
-  { no: '13', name: '핸드메이드' },
-  { no: '14', name: '취미' },
-  { no: '15', name: '생활서비스' },
-  { no: '16', name: '기타' },
-];

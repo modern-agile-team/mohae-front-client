b13e7c07ebdf55d93e21f06c0ded343501c1ef76
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-24 21:57:58 +0900

    [Refactor] Filter Component에 자체적인 onSubmit 생성 #242

--- app/src/components/filter/Container.tsx
+++ app/src/components/filter/Container.tsx
@@ -6,34 +6,134 @@ import Presenter from './Presenter';
 import {
   ContainerProps,
   FilterInitialState,
+  ObjDataProcessing,
 } from '../../types/searchComponent/filter/type';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 interface SetItemCheckParams {
   key: 'target' | 'sort' | 'date' | 'free';
   index: number;
 }
 
-function Filter({ setShowFilter, onSubmit }: ContainerProps) {
+function Filter({ setShowFilter }: ContainerProps) {
   const dispatch = useDispatch();
-  const filterData: FilterInitialState = useSelector(
+  const { check, area, price }: FilterInitialState = useSelector(
     (state: RootState) => state.filter.data,
   );
+  const { no } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const getParams = (query: string): any => {
+    return searchParams.get(query);
+  };
+  const changeNull = (filteringValue: boolean | number | string) => {
+    if (
+      filteringValue === false ||
+      filteringValue === 0 ||
+      filteringValue === 1000000
+    ) {
+      return null;
+    } else return filteringValue;
+  };
+
+  const objDataProcessing: ObjDataProcessing = {
+    check: {
+      sort: {
+        1: changeNull(check.sort[0]),
+        DESC: changeNull(check.sort[1]),
+        ASC: changeNull(check.sort[2]),
+      },
+      target: {
+        0: changeNull(check.target[0]),
+        1: changeNull(check.target[1]),
+      },
+      date: {
+        7: changeNull(check.date[0]),
+        30: changeNull(check.date[1]),
+        60: changeNull(check.date[2]),
+        0: changeNull(check.date[3]),
+      },
+      free: { 1: changeNull(check.free[0]) },
+    },
+    area: {
+      areaNo: changeNull(area.areaNo),
+    },
+    price: {
+      min: changeNull(price.min),
+      max: changeNull(price.max),
+    },
+  };
+
+  const sortQuery = () => {
+    if (objDataProcessing.check.sort[1] !== null) {
+      return '&popular=1';
+    } else
+      return (
+        '&sort=' +
+        Object.keys(objDataProcessing.check.sort)
+          .map((el, i) => {
+            if (objDataProcessing.check.sort[el] !== null) {
+              return Object.keys(objDataProcessing.check.sort)[i];
+            }
+          })
+          .filter(el => el)[0]
+      );
+  };
+
+  const drawObjKey = (obj: any) => {
+    const value = Object.keys(obj)
+      .map((el, i) => {
+        if (obj[el] !== null) {
+          return Object.keys(obj)[i];
+        }
+      })
+      .filter(el => el)[0];
+    return value ? value : null;
+  };
+
+  const handlePriceQuery = () => {
+    return {
+      min:
+        objDataProcessing.check.free[1] === null
+          ? objDataProcessing.price.min
+          : null,
+      max:
+        objDataProcessing.check.free[1] === null
+          ? objDataProcessing.price.max
+          : null,
+    };
+  };
+
+  const onSubmitTest = (
+    e: React.MouseEvent | React.FormEvent<HTMLFormElement> | React.ChangeEvent,
+  ) => {
+    const query = `?categoryNo=${no}&title=${getParams(
+      'title',
+    )}${sortQuery()}&target=${drawObjKey(
+      objDataProcessing.check.target,
+    )}&date=${drawObjKey(objDataProcessing.check.date)}&free=${drawObjKey(
+      objDataProcessing.check.free,
+    )}&min=${handlePriceQuery().min}&max=${handlePriceQuery().max}&areaNo=${
+      objDataProcessing.area.areaNo
+    }`;
+    e.preventDefault();
+
+    setSearchParams(query);
+  };
 
   const setItemCheck = (param: SetItemCheckParams) => {
-    const resetValue = Object.keys(filterData.check[param.key]).map(
-      (_: string, __) => {
-        return false;
-      },
-    );
+    const resetValue = Object.keys(check[param.key]).map((_: string, __) => {
+      return false;
+    });
 
     const newItem = {
-      sort: { ...filterData.check.sort },
-      target: { ...filterData.check.target },
-      date: { ...filterData.check.date },
-      free: { ...filterData.check.free },
+      sort: { ...check.sort },
+      target: { ...check.target },
+      date: { ...check.date },
+      free: { ...check.free },
       [param.key]: {
         ...resetValue,
-        [param.index]: !filterData.check[param.key][param.index],
+        [param.index]: !check[param.key][param.index],
       },
     };
     dispatch(setCheck(newItem));
@@ -42,7 +142,7 @@ function Filter({ setShowFilter, onSubmit }: ContainerProps) {
   return (
     <Presenter
       setItemCheck={setItemCheck}
-      onSubmit={onSubmit}
+      onSubmit={onSubmitTest}
       setShowFilter={setShowFilter}
     />
   );

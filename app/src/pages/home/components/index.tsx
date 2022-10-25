26e10d7759ca20b792aa59bd1e67f039a4a52722
tjdwp0211 <rlatjdwp0211@naver.com>
2022-10-25 16:27:01 +0900

    [Refactor] Main Page 폴더 구조 변경 후 컴포넌트명 변경 #292

--- app/src/pages/home/components/index.tsx
+++
@@ -1,6 +0,0 @@
-import FirstPage from './firstPage/FirstPage';
-import SecondPage from './secondPage/SecondPage';
-import ThirdPage from './thirdPage/ThirdPage';
-import FourthPage from './fourthPage/FourthPage';
-
-export { FirstPage, SecondPage, ThirdPage, FourthPage };

effca54fc3a09390c5c9e4d3362027c31d0871e1
baebumsoo <baebeomsu77@gmail.com>
2022-10-31 17:07:16 +0900

    Refactor(feature/bumsoo/#311): 스타일 코드 리팩토링

--- app/src/pages/login/register/Agreement.tsx
+++ app/src/pages/login/register/Agreement.tsx
@@ -150,9 +150,6 @@ const CheckItem = styled.div`
   }
 `;
 const CheckBox = styled.div`
-  width: 16px;
-  height: 16px;
-  border-radius: 2px;
   background-color: #ffffff;
   box-shadow: 0px 0px 4px 0px #84838d;
   cursor: pointer;

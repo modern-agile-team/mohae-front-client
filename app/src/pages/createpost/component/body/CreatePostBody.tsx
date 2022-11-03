90f9666a2cb511e4147c84504a4322810a2a4092
tjdwp0211 <rlatjdwp0211@naver.com>
2022-11-03 16:54:34 +0900

    [Refactor] 게시글 작성에서 쓰는 페이지 컴포넌트 리펙터링 #261

--- app/src/pages/createpost/component/body/CreatePostBody.tsx
+++ app/src/pages/createpost/component/body/CreatePostBody.tsx
@@ -1,54 +1,22 @@
 import React from 'react';
-import { css, cx } from '@emotion/css';
+import styled from '@emotion/styled';
 import { Box, TextEditor } from '../../../../components';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../../redux/root';
-import { PostData } from '../../../../types/post/type';
 
-// 본문, 거래 참고 사항
-interface PostBodyProps {
-  data?: PostData;
-  view?: boolean;
-  getValue?: boolean;
-  type?: string;
-}
-function PostBody(props: PostBodyProps) {
-  const { view } = props;
-  const reduxData = useSelector((state: RootState) => state.post.data.response);
-
-  const body = () => {
-    return view ? (
-      <div className="description-scrollBox">
-        <div
-          className="description-box"
-          dangerouslySetInnerHTML={{
-            __html:
-              reduxData.board.description && reduxData.authorization
-                ? reduxData.board.description
-                : '게시글 상세 조회는 로그인을 하고 이용 하시길 바랍니다.',
-          }}
-        />
-      </div>
-    ) : (
-      <TextEditor size={379} />
-    );
-  };
-
+function PostBody() {
   return (
-    <div className={cx(style(view))}>
+    <Wrapper>
       <Box size={[1128, 397]} className="body">
-        {body()}
+        <TextEditor size={379} />
       </Box>
-    </div>
+    </Wrapper>
   );
 }
 
 export default PostBody;
 
-const style = (view?: boolean) => css`
-  margin: ${view ? '32px 0px 16px 0px' : '32px 0px 64px 0px'};
+const Wrapper = styled.div`
+  margin: 32px 0px 64px 0px;
   .body {
-    padding: ${view && '16px 0px 16px 24px'};
     font-size: 14px;
     font-family: 'Regular';
     word-break: break-all;

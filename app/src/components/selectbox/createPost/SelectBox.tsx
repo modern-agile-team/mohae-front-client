27e8426198566076f1b90f42d7a6eb9a93a97a68
tjdwp0211 <rlatjdwp0211@naver.com>
2022-11-09 22:33:27 +0900

    [Refactor] SelectBox 리펙터링 #261

--- app/src/components/selectbox/createPost/SelectBox.tsx
+++ app/src/components/selectbox/createPost/SelectBox.tsx
@@ -1,61 +1,62 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import SelectList from './SelectList';
-import { color, font, shadow } from '../../../styles';
 import Img from '../../img/Img';
-import { WhiteButton } from '../../button';
 import styled from '@emotion/styled';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/root';
+import { useParams } from 'react-router-dom';
+import { lists } from '../../../consts/categoryList';
 
-interface Contents {
-  no: string;
-  name: string;
-}
 interface Props {
   view: boolean;
   handleView: () => void;
-  placeholder?: string;
-  style: string;
+  using: string;
+  selected: boolean;
 }
 
 function SelectBox(props: Props) {
-  const { view, handleView, placeholder, style } = props;
-  const { deadline, areaNo, categoryNo } = useSelector(
-    (state: RootState) => state.createPost.data,
-  );
+  const { view, handleView, using, selected } = props;
+  const { no } = useParams();
+  const reduxData: { [key: string]: number | string | null | string[] } =
+    useSelector((state: RootState) => state.createPost.data);
 
-  const [selected, setSelected] = useState<string>(
-    placeholder ? placeholder : '전체지역',
-  );
+  const initialPlaceholder = useMemo(() => {
+    return lists[using]
+      .map(list => Number(list.no) === Number(reduxData[using]) && list.name)
+      .filter(el => el)[0];
+  }, []);
 
-  const contentsStyle = () => {
-    return (
-      <div className="placeholderWrap">
-        <div className="category">
-          {style !== '카테고리' ? (
-            <WhiteButton able>{selected}</WhiteButton>
-          ) : (
-            { selected }
-          )}
-        </div>
-      </div>
-    );
+  const handleInitialPlaceholder = () => {
+    switch (using) {
+      case 'categoryNo':
+        return '카테고리';
+      case 'areaNo':
+        return '전체 지역';
+      case 'deadline':
+        return '기간';
+    }
   };
+
+  const [placeholder, setPlaceholder] = useState(
+    no ? initialPlaceholder : handleInitialPlaceholder(),
+  );
 
   return (
     <>
-      <Wrap view={view} onClick={handleView}>
-        {contentsStyle()}
-        <div className="opener">
-          <Img src="/img/arrow-down-dark3.png" />
-        </div>
+      <Wrap selected={selected} view={view} onClick={handleView}>
+        <div className="placeholder-wrap">{placeholder}</div>
+        <Opener>
+          <Img
+            src={view ? '/img/arrow-up-dark3.png' : '/img/arrow-down-dark3.png'}
+          />
+        </Opener>
       </Wrap>
       {view && (
         <>
           <SelectList
-            contents={list[style]}
-            style={style}
-            setSelected={setSelected}
+            contents={lists[using]}
+            using={using}
+            setPlaceholder={setPlaceholder}
             handleView={handleView}
           />
           <Overlay onClick={handleView} />
@@ -76,76 +77,29 @@ const Overlay = styled.div`
   height: 100vh;
 `;
 
-const Wrap = styled.div<{ view: boolean }>`
+const Wrap = styled.div<{ view: boolean; selected: boolean }>`
+  border-radius: 6px 6px 0px 0px;
+  box-shadow: ${props => props.view && '0px 0px 4px rgba(132, 131, 141, 0.25)'};
+  border-bottom: 2px solid #e7e7e8;
   position: relative;
-  ${shadow.button}
-  ${props => (props.view ? 'border-radius: 6px 6px 0px 0px;' : '6px')}
-      padding: 0px 8px;
-  .placeholderWrap {
-    width: 106px;
-    display: flex;
-    justify-content: center;
-  }
+  padding: 0px 8px;
   width: 368px;
   height: 62px;
   display: flex;
   align-items: center;
-  ${font.size[14]}
-  ${font.weight[400]}
+  font-size: 14px;
+  font-family: 'Regular';
   cursor: pointer;
-  .opener {
-    width: 24px;
-    height: 24px;
-  }
-  .category {
-    width: 100px;
-    height: 36px;
-  }
-  .placeholderWrap {
+
+  .placeholder-wrap {
     width: 344px;
     display: flex;
-    align-items: center;
-  }
-  .placeholder {
-    color: ${color.dark3};
+    justify-content: center;
+    color: ${props => (props.selected ? '#4f4e5c' : '#a7a7ad')};
   }
 `;
 
-const list: { [placeholder: string]: Contents[] } = {
-  카테고리: [
-    { no: '1', name: '디자인' },
-    { no: '2', name: 'IT/개발' },
-    { no: '3', name: '사진/영상' },
-    { no: '4', name: '기획/마케팅' },
-    { no: '5', name: '번역/통역' },
-    { no: '6', name: '문서작업' },
-    { no: '7', name: '컨설팅' },
-    { no: '8', name: '법률' },
-    { no: '9', name: '과외/레슨' },
-    { no: '10', name: '상담/운세' },
-    { no: '11', name: '이벤트' },
-    { no: '12', name: '핸드메이드' },
-    { no: '13', name: '취미' },
-    { no: '14', name: '생활서비스' },
-    { no: '15', name: '기타' },
-  ],
-  '전체 지역': [
-    { no: '1', name: '서울' },
-    { no: '2', name: '경기도' },
-    { no: '3', name: '강원도' },
-    { no: '4', name: '대전' },
-    { no: '5', name: '세종시' },
-    { no: '6', name: '전라도' },
-    { no: '7', name: '광주' },
-    { no: '8', name: '경상도' },
-    { no: '9', name: '부산' },
-    { no: '10', name: '울산' },
-    { no: '11', name: '제주도' },
-  ],
-  기간: [
-    { no: '7', name: '일주일' },
-    { no: '30', name: '1개월' },
-    { no: '60', name: '3개월' },
-    { no: '0', name: '상시' },
-  ],
-};
+const Opener = styled.div`
+  width: 24px;
+  height: 24px;
+`;
--- app/src/components/selectbox/createPost/SelectList.tsx
+++ app/src/components/selectbox/createPost/SelectList.tsx
@@ -1,7 +1,5 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import { css, cx } from '@emotion/css';
-import { Btn } from '../../button';
-import { color, shadow } from '../../../styles';
+import styled from '@emotion/styled';
 import { useDispatch } from 'react-redux';
 import {
   setAreaNum,
@@ -15,103 +13,85 @@ interface Contents {
 }
 interface Props {
   contents: Contents[];
-  style: string;
-  setSelected: Dispatch<SetStateAction<string>>;
+  using: string;
+  setPlaceholder: Dispatch<SetStateAction<string | false | undefined>>;
   handleView: () => void;
 }
 
+interface OnClickType {
+  [key: string]: (e: React.MouseEvent, selected: string) => void;
+}
+
 function SelectList(props: Props) {
-  const { contents, style, setSelected, handleView } = props;
+  const { contents, using, setPlaceholder, handleView } = props;
   const dispatch = useDispatch();
 
-  const wrap = css`
-    position: relative;
-    background-color: white;
-    z-index: 5;
-    .container {
-      width: 368px;
-      height: 247px;
-      width: 368px;
-      height: 247px;
-      position: absolute;
-      top: 0px;
-      left: 0px;
-      overflow-y: scroll;
-      background-color: white;
-      border-radius: 0px 0px 6px 6px;
-      ${shadow.button}
-    }
-    .category {
-      width: 100px;
-      height: 36px;
-    }
-    ul {
-      width: 368px;
-      height: 62px;
-      padding-right: 20px;
-      cursor: pointer;
-      background-color: white;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      color: ${color.dark3};
-      :hover {
-        background-color: ${color.subtle};
-        color: ${color.dark1};
-      }
-    }
-  `;
-
-  const onClick = {
-    area: (e: React.MouseEvent, selected: string) => {
+  const onClick: OnClickType = {
+    categoryNo(e: React.MouseEvent, selected: string) {
+      dispatch(setCategoryNum(e.currentTarget.id));
+      setPlaceholder(selected);
+      handleView();
+    },
+    areaNo(e: React.MouseEvent, selected: string) {
       dispatch(setAreaNum(e.currentTarget.id));
-      setSelected(selected);
+      setPlaceholder(selected);
       handleView();
     },
-    date: (e: React.MouseEvent, selected: string) => {
+    deadline(e: React.MouseEvent, selected: string) {
       dispatch(setDeadline(e.currentTarget.id));
-      setSelected(selected);
-      handleView();
-    },
-    category: (e: React.MouseEvent, selected: string) => {
-      dispatch(setCategoryNum(Number(e.currentTarget.id) + 1));
-      setSelected(selected);
+      setPlaceholder(selected);
       handleView();
     },
   };
 
   const lists = () => {
-    switch (style) {
-      case '카테고리':
-        return contents.map((el, i: any) => (
-          <ul key={i} id={i} onClick={e => onClick.category(e, el.name)}>
-            <div className="category">
-              <Btn white category>
-                {el.name}
-              </Btn>
-            </div>
-          </ul>
-        ));
-      case '전체 지역':
-        return contents.map((el, i: any) => (
-          <ul key={i} id={el.no} onClick={e => onClick.area(e, el.name)}>
-            {el.name}
-          </ul>
-        ));
-      case '기간':
-        return contents.map((el, i: any) => (
-          <ul key={i} id={el.no} onClick={e => onClick.date(e, el.name)}>
-            {el.name}
-          </ul>
-        ));
-    }
+    return contents.map((el, i: number) => (
+      <List key={i} id={el.no} onClick={e => onClick[using](e, el.name)}>
+        {el.name}
+      </List>
+    ));
   };
 
   return (
-    <div className={cx(wrap)}>
-      <div className="container">{lists()}</div>
-    </div>
+    <Wrapper>
+      <Container>{lists()}</Container>
+    </Wrapper>
   );
 }
 
 export default SelectList;
+
+const Wrapper = styled.div`
+  position: relative;
+  background-color: white;
+  z-index: 5;
+`;
+
+const Container = styled.div`
+  width: 368px;
+  height: 247px;
+  width: 368px;
+  height: 247px;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow-y: scroll;
+  border-radius: 0px 0px 6px 6px;
+  box-shadow: 0px 0px 4px rgba(132, 131, 141, 0.25);
+`;
+
+const List = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 368px;
+  height: 62px;
+  padding-right: 20px;
+  cursor: pointer;
+  background-color: white;
+  color: #a7a7ad;
+  :hover {
+    background-color: #fcf3f4;
+    color: #4f4e5c;
+  }
+`;

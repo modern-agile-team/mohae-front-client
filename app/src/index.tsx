0cb30f7588f1641c6a69f39cb2cef537734905f6
tjdwp0211 <rlatjdwp0211@naver.com>
2022-11-12 17:34:52 +0900

    [Delete] 불필요한 import 삭제 #375

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,7 +1,5 @@
 /** @format */
 
-import * as React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';

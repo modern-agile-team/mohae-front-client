3b1b368fa68ba6754d0e5a1cd46b50d8167b62c8
tjdwp0211 <rlatjdwp0211@naver.com>
2022-11-16 15:41:43 +0900

    [Refactor] Popup 전역 상태관리 댓글에 반영 #387

--- app/src/components/comment/CommentList.tsx
+++ app/src/components/comment/CommentList.tsx
@@ -1,23 +1,14 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/root';
-import { CommentListProps } from '../../types/comment/type';
 import CommentItem from './CommentItem';
 
-const CommentList = (props: CommentListProps) => {
-  const { handleModalView, handlePopupView } = props;
+const CommentList = () => {
   const commentList = useSelector((state: RootState) => state.comment.data);
 
   return (
     <ul>
       {commentList.map((_, i) => {
-        return (
-          <CommentItem
-            key={i}
-            commentIndex={i}
-            handleModalView={handleModalView}
-            handlePopupView={handlePopupView}
-          />
-        );
+        return <CommentItem key={i} commentIndex={i} />;
       })}
     </ul>
   );
